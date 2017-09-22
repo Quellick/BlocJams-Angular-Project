b105bb3c72762f555fae3e09b8ed2ff1828499ae
Quellick <Quellick@gmail.com>
2017-09-22 12:41:17 -0700

    commit
    checkpoint work finished unsure if its correct

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           var SongPlayer = {};
 
           var currentAlbum = Fixtures.getAlbum();
@@ -23,7 +23,14 @@
               currentBuzzObject = new buzz.sound(song.audioUrl, {
                      formats: ['mp3'],
                      preload: true
-    });
+              });
+
+              currentBuzzObject.bind('timeupdate', function() {
+                  $rootScope.$apply(function() {
+                      SongPlayer.currentTime = currentBuzzObject.getTime();
+                  });
+              });
+
 
     SongPlayer.currentSong = song;
  };
@@ -55,6 +62,11 @@
 * @type {Object}
 */
           SongPlayer.currentSong = null;
+/*
+* @desc Current playback time (in seconds) of currently playing song
+* @type {Number}
+*/
+          SongPlayer.currentTime = null;
 /*
 *@public function SongPlayer.play
 *@desc plays the selected Buzz object if its not already playing and sets the playing property of the song object to true
@@ -113,10 +125,21 @@
             }
         };
 
+/*
+* @function setCurrentTime
+* @desc Set current time (in seconds) of currently playing song
+* @param {Number} time
+*/
+      SongPlayer.setCurrentTime = function(time) {
+          if (currentBuzzObject) {
+          currentBuzzObject.setTime(time);
+          }
+        };
+
           return SongPlayer;
         }
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
